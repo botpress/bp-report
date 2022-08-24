8c86bb828f14e4930815d48e83af87ca919c8854
Patrick <87815239+ptrckbp@users.noreply.github.com>
2022-08-24 14:27:48 -0400

    changes to history mode all the time

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,5 @@
 import { route } from 'quasar/wrappers'
-import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
+import { createRouter,  createWebHistory } from 'vue-router'
 import routes from './routes'
 
 /*
@@ -12,9 +12,7 @@ import routes from './routes'
  */
 
 export default route(function (/* { store, ssrContext } */) {
-  const createHistory = process.env.SERVER
-    ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
+  const createHistory = createWebHistory
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
